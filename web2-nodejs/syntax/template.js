618ee48b0241dcc0bc737a93db922ee52559a363
eunryong <studyeunryong@gmail.com>
2022-04-11 20:26:38 +0900

    Template literals 배움

---
+++ web2-nodejs/syntax/template.js
@@ -0,0 +1,10 @@
+var name = 'eunryong';
+
+// String literals
+var letter = 'Dear '+name+'\n\n Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati dolores eos, itaque assumenda, illo ullam enim at animi, quia tenetur ipsam exercitationem minima pariatur. Tenetur vitae itaque aspernatur eius cum?' +name;
+
+// Template literals
+var letter = `Dear ${name}
+
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam, ipsam! Rerum sint unde quas corporis recusandae animi perferendis molestiae. ${name}Eum numquam eligendi incidunt ${name} rerum sed illum sit deleniti quod vitae.`
+console.log(letter);
