8a8e75cd1ab9dd064ecc1afd3f50dbcf1f88db0a
eunryong <studyeunryong@gmail.com>
2022-04-25 20:37:59 +0900

    session 객체

--- nodejs cookie/Nodejs-master/nodejs/express-session.js
+++ nodejs cookie/Nodejs-master/nodejs/express-session.js
@@ -5,31 +5,20 @@ var session = require('express-session')
 var app = express()
   
 app.use(session({
-  secret: 'keyboard cat',
+  secret: 'dsaet53!$eq14',
   resave: false,
   saveUninitialized: true
 }))
   
-app.use(function (req, res, next) {
-  if (!req.session.views) {
-    req.session.views = {}
-  }
   
-  // get the url pathname
-  var pathname = parseurl(req).pathname
-  
-  // count the views
-  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
-  
-  next()
-})
-  
-app.get('/foo', function (req, res, next) {
-  res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
-})
-  
-app.get('/bar', function (req, res, next) {
-  res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
+app.get('/', function (req, res, next) {
+    console.log(req.session);
+    if(req.session.num === undefined){
+        req.session.num = 1;
+    } else {
+        req.session.num =  req.session.num + 1;
+    }
+    res.send(`Views : ${req.session.num}`);
 })
  
 app.listen(3000, function(){
